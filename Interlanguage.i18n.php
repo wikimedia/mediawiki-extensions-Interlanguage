47fc833966248726887bb3a9dc3a05dd386d53a3
Raimond Spekking <raymond@users.mediawiki.org>
2012-03-18 20:13:19 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- Interlanguage.i18n.php
+++ Interlanguage.i18n.php
@@ -570,10 +570,12 @@ $messages['yi'] = array(
 
 /** Simplified Chinese (‪中文(简体)‬)
  * @author Hydra
+ * @author Linforest
  */
 $messages['zh-hans'] = array(
 	'interlanguage-desc' => '抓住从另一个维基语链接',
 	'interlanguage-pagelinksexplanation' => '中介语的链接的页：',
+	'interlanguage-editlinks' => '编辑链接',
 );
 
 /** Traditional Chinese (‪中文(繁體)‬) */
